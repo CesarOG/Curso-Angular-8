a2d942e73e6a02676b1b7644e470f91f3d269150
mcortega <ortegapb@gmail.com>
2019-07-11 18:24:19 -0500

    Creacion y configuracion de la Aplicacion 2 e introduccion a manejo de rutas

---
+++ 02-spa/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+
+//Rutas
+import { APP_ROUTING } from "./app.routes";
+//Servicios
+
+// Componentes
+import { AppComponent } from "./app.component";
+import { NavbarComponent } from "./components/shared/navbar/navbar.component";
+import { HomeComponent } from "./components/home/home.component";
+import { AboutComponent } from "./components/about/about.component";
+import { HeroesComponent } from "./components/heroes/heroes.component";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    HeroesComponent
+  ],
+  imports: [BrowserModule, APP_ROUTING],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
