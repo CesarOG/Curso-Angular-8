82e6265b230a8830c2f3f60d06d961b5b19c2a06
mcortega <ortegapb@gmail.com>
2019-07-12 23:35:39 -0500

    Introduccion a los Pipes

---
+++ 03-pipes/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  nombre = "César";
+  arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  PI = Math.PI;
+}
