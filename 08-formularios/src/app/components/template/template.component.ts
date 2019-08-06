d355488f5b14b5901369c27df0e16e26dc47cfdf
mcortega <ortegapb@gmail.com>
2019-08-05 23:25:15 -0500

    Creando Formularios(Template y Data).se trabajo con los formularios con logica en el html(Template)

---
+++ 08-formularios/src/app/components/template/template.component.ts
@@ -0,0 +1,26 @@
+import { Component } from "@angular/core";
+import { NgForm } from "@angular/forms";
+
+@Component({
+  selector: "app-template",
+  templateUrl: "./template.component.html",
+  styles: []
+})
+export class TemplateComponent {
+  usuario: object = {
+    nombre: null,
+    apellido: null,
+    correo: null,
+    pais:"",
+    sexo:"Hombre",
+    acepta:true
+  };
+  paises:object=[{codigo:"PE",nombre:"Perú"},{codigo:"BR",nombre:"Brasil"}];
+  sexos:string[]=["Hombre","Mujer"]
+  constructor() {}
+
+  guardar(forma: NgForm) {
+    console.log(forma);
+    console.log(this.usuario);
+  }
+}
